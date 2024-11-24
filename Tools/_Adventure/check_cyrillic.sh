c051ce9f765bb6e541f7b40430b67ad33673c971
c4llv07e <igor@c4llv07e.xyz>
2024-11-24 04:32:11 +0300

    Better cyrillic check

--- Tools/_Adventure/check_cyrillic.sh
+++ Tools/_Adventure/check_cyrillic.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 set -xue
 (! git grep -IP -e "([a-zA-Z][а-яА-Я]|[а-яА-Я][a-zA-Z])" --and --not -e "\\\n[а-яА-Я]" "${GITHUB_BASE_REF}")
-(! git ls-tree --full-tree -r "${GITHUB_BASE_REF}" | grep -P "[а-яА-Я]")
+(! git -c core.quotepath=off ls-tree --full-tree -r -t --name-only --full-name "${GITHUB_BASE_REF}" | grep -P "[а-яА-Я]")
