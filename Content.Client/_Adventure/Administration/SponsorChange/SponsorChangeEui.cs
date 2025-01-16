3552a60b2785b105c5e73044ee61793013aebd37
c4llv07e <igor@c4llv07e.xyz>
2025-01-16 12:18:10 +0300

    Moar stashes

--- Content.Client/_Adventure/Administration/SponsorChange/SponsorChangeEui.cs
+++ Content.Client/_Adventure/Administration/SponsorChange/SponsorChangeEui.cs
@@ -7,15 +7,14 @@ namespace Content.Client._Adventure.Administration.SponsorChange;
 [UsedImplicitly]
 public sealed class SponsorChangeEui : BaseEui
 {
-    [Dependency] private readonly IServerDbManager _db = default!;
-
     private readonly SponsorChangeWindow _window;
 
     public SponsorChangeEui()
     {
         _window = new SponsorChangeWindow();
-        _window.OnUsernameEntered += OnUsernameEntered;
-        _window.OnTierSelected += OnTierSelected;
+        _window.OnUsernameEntered += (username) => SendMessage(GetPlayerSponsorInfoRequest(username));
+        _window.OnTierSelected += (tier) => SetSponsorTierRequest(_window.GetUsername(), tier);
+        _window.OnClose += () => SendMessage(new CloseEuiMessage());
     }
 
     public override void Opened()
@@ -31,25 +30,36 @@ public sealed class SponsorChangeEui : BaseEui
 
     private void OnUsernameEntered(string username)
     {
-        var player = _db.GetPlayerRecordByUserName(username);
-        if (player == null)
-        {
-            _window.SetMenuEnabled(false);
-            return;
-        }
-        _window.SetMenuEnabled(true);
-        _window.SelectSponsorTier(player.SponsorTier ?? null);
+        // var player = _db.GetPlayerRecordByUserName(username);
+        // if (player == null)
+        // {
+        //     _window.SetMenuEnabled(false);
+        //     return;
+        // }
+        // _window.SetMenuEnabled(true);
+        // _window.SelectSponsorTier(player.SponsorTier ?? null);
     }
 
     private void OnTierSelected(string? tier)
     {
-        var player = _db.GetPlayerRecordByUserName(_window.GetUsername());
-        if (player == null)
-        {
-            // По-идее такого быть не должно, но на всякий случай.
-            _window.SetMenuEnabled(false);
+        // var player = _db.GetPlayerRecordByUserName(_window.GetUsername());
+        // if (player == null)
+        // {
+        //     // По-идее такого быть не должно, но на всякий случай.
+        //     _window.SetMenuEnabled(false);
+        //     return;
+        // }
+        // _db.SetPlayerRecordSponsor(player.UserId, tier);
+    }
+
+    public override void HandleState(EuiStateBase state)
+    {
+        if (state is not SponsorChangeEuiState s)
             return;
-        }
-        _db.SetPlayerRecordSponsor(player.UserId, tier);
+
+        _window.PlayerNameLine.Text = s.Username;
+        _window.SetMenuEnabled(s.IsValid);
+        if (s.IsValid)
+            _window.SelectSponsorTier(s.Tier ?? null);
     }
 }
--- Content.Server/_Adventure/Administration/SponsorChange/SponsorChangeEui.cs
+++ Content.Server/_Adventure/Administration/SponsorChange/SponsorChangeEui.cs
@@ -7,20 +7,35 @@ namespace Content.Server._Adventure.Administration.SponsorChange;
 
 public sealed class SponsorChangeEui : BaseEui
 {
+    [Dependency] private readonly IServerDbManager _db = default!;
+    [Dependency] private readonly ILogManager _log = default!;
+    [Dependency] private readonly IAdminManager _admins = default!;
+
     public SponsorChangeEui()
     {
+        IoCManager.InjectDependencies(this);
+        _sawmill = _log.GetSawmill("sponsor.changes");
     }
 
     public override void HandleMessage(EuiMessageBase msg)
     {
         base.HandleMessage(msg);
+        switch (msg)
+        {
+            case SponsorChangeEuiStateMsg.SetSponsorTierRequest r:
+                SetSponsor(r.Username, r.Tier);
+                break;
+            case SponsorChangeEuiStateMsg.GetPlayerSponsorInfoRequest r:
+                GetSponsorInfo(r.Username);
+                break;
+        }
+    }
 
-        // if (msg is not SponsorChangeMessage change)
-        // {
-        //     Close();
-        //     return;
-        // }
+    public void SetSponsor(string username, ProtoId<SponsorTierPrototype>? tier)
+    {
+    }
 
-        Close();
+    public void GetSponsorInfo(string username)
+    {
     }
 }
